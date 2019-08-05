126e8e4f540aa0542ad544b8737143c532227c26
Aleksander Alekseev <mail@eax.me>
2019-08-05 18:08:42 +0300

    Optimize queue memory footprint

--- actor/mailbox/queue.go
+++ actor/mailbox/queue.go
@@ -2,22 +2,26 @@ package mailbox
 
 import "github.com/afiskon/go-actors/actor"
 
-type queue struct {
-	len int
-	next *queue
-	prev *queue
+type queue_item struct {
+	next *queue_item
+	prev *queue_item
 	payload actor.Message
 }
 
+type queue struct {
+	len int
+	head queue_item
+}
+
 func (q *queue) init() {
-	q.next = q
-	q.prev = q
-	q.payload = nil
+	q.head.next = &q.head
+	q.head.prev = &q.head
+	q.head.payload = nil
 	q.len = 0
 }
 
 func (q *queue) empty() bool {
-	return q.next == q
+	return q.head.next == &q.head
 }
 
 func (q *queue) length() int {
@@ -25,24 +29,24 @@ func (q *queue) length() int {
 }
 
 func (q *queue) enqueue(payload actor.Message) {
-	new :=  &queue{
+	new :=  &queue_item{
 		payload: payload,
 	}
 
-	new.next = q.next
-	new.prev = q
-	q.next.prev = new
-	q.next = new
+	new.next = q.head.next
+	new.prev = &q.head
+	q.head.next.prev = new
+	q.head.next = new
 	q.len++
 }
 
 // empty() should be checked before this call
 func (q *queue) dequeue() actor.Message {
-	msg := q.prev.payload
-	old := q.prev
+	msg := q.head.prev.payload
+	old := q.head.prev
 
-	old.prev.next = q
-	q.prev = old.prev
+	old.prev.next = &q.head
+	q.head.prev = old.prev
 	old.next = nil
 	old.prev = nil
 	q.len--
@@ -54,10 +58,10 @@ func (q1 *queue) moveFromQueue(q2* queue) {
 		return
 	}
 
-	q2.next.prev = q1.prev
-	q1.prev.next = q2.next
-	q2.prev.next = q1
-	q1.prev = q2.prev
+	q2.head.next.prev = q1.head.prev
+	q1.head.prev.next = q2.head.next
+	q2.head.prev.next = &q1.head
+	q1.head.prev = q2.head.prev
 
 	q1.len += q2.len
 	q2.init()
